ce4711b176cc2030958e4680abbe3f7158259045
bfletcher <bfletch@gmail.com>
2018-12-11 20:01:51 -0500

    Rough solution to day 1

--- src/day01/index.test.ts
+++ src/day01/index.test.ts
@@ -1,9 +1,40 @@
 import { assert } from "chai";
 import "mocha";
-import { add } from ".";
+import {
+    day01,
+    day01StartFromString,
+    solvesTheProblem,
+    parseTextFile
+} from ".";
 
-describe("add:", () => {
-    it("add(1 + 1) = 2", () => {
-        assert.equal(add(1, 1), 2);
+describe("Day 01:", () => {
+    it("Sums [1, 2, 3] and equal 6", () => {
+        assert.equal(day01([1, 2, 3]), 6);
+    });
+
+    it("Sums string and equal 6", () => {
+        assert.equal(day01StartFromString("+1\n+2\n+3"), 6);
+    });
+
+    it("Sums default input and gives answer", () => {
+        assert.equal(day01StartFromString(), 474);
+    });
+});
+
+describe("Day 01B:", () => {
+    it("Finds first repeat for -1, 1 which is 0", () => {
+        assert.equal(solvesTheProblem([-1, 1]), 0);
+    });
+    it("Finds first repeat for 2, 1, -1 which is 2", () => {
+        assert.equal(solvesTheProblem([2, 1, -1]), 2);
+    });
+    it("Finds first repeat for 1, -2 which is 2", () => {
+        assert.equal(solvesTheProblem([1, -2]), 0);
+    });
+    it("Finds first repeat for input", () => {
+        assert.equal(solvesTheProblem(parseTextFile("input")), 137041);
+    });
+    it("Finds first repeat for input-ben", () => {
+        assert.equal(solvesTheProblem(parseTextFile("input-ben")), 56752);
     });
 });
--- src/day01/index.ts
+++ src/day01/index.ts
@@ -1,3 +1,43 @@
-export function add(a: number, b: number): number {
-    return a + b;
+const fs = require("fs");
+
+const inputText = fs.readFileSync("./src/day01/input.txt", "UTF-8");
+
+export function day01StartFromString(numbersText: string = inputText): number {
+    const numArray = numbersText.split("\n").map(Number);
+
+    let ourAnswer = day01(numArray);
+
+    return ourAnswer;
+}
+
+export function day01(numsList: number[]): number {
+    return numsList.reduce((sum: number, value: number) => sum + value, 0);
+}
+
+export function parseTextFile(fileName: string = "input"): number[] {
+    const theNumbers = fs
+        .readFileSync(`./src/day01/${fileName}.txt`, "UTF-8")
+        .split("\n")
+        .map(Number);
+
+    return theNumbers;
+}
+
+export function solvesTheProblem(theNumbers: number[]): number {
+    const seenFrequencies = new Set();
+    let currentFrequency = 0;
+    let i = 0;
+    let numResets = 0;
+    while (!seenFrequencies.has(currentFrequency)) {
+        seenFrequencies.add(currentFrequency);
+        if (i >= theNumbers.length) {
+            i = 0;
+            numResets++;
+        }
+        currentFrequency += theNumbers[i];
+        i++;
+    }
+    console.log("Resets", numResets);
+
+    return currentFrequency;
 }
